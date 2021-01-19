17cc1b2f258ca48de4ddab5d3e912b4777fe970b
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2021-01-19 10:27:35 -0300

    configured async storage to save some infos

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from 'styled-components/native';
 import Router from './router';
 import store from './store';
 import { hideErrorMessage } from '~/services/auth';
+import { onboardingIsFinished } from '~/services/onboarding';
+import { getCurrentTheme } from '~/services/theme';
 import {
   Billboard,
   NOTIFICATIONS_POSITION,
@@ -20,11 +22,16 @@ const AppWrapper = () => {
 
 const App = () => {
   const { error, errorMessage } = useSelector((state) => state.auth);
+  const { done } = useSelector((state) => state.onboarding);
   const currentTheme = useSelector((state) => state.theme.current);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('ERROR HERE', error);
+    dispatch(onboardingIsFinished());
+    dispatch(getCurrentTheme());
+  }, []);
+
+  useEffect(() => {
     if (error) {
       showErrorNotification({
         text: errorMessage,
@@ -39,7 +46,7 @@ const App = () => {
 
   return (
     <ThemeProvider theme={currentTheme}>
-      <Router />
+      <Router hasOnboarding={!done} />
       <Billboard position={NOTIFICATIONS_POSITION.TOP} />
     </ThemeProvider>
   );
--- src/services/theme.js
+++ src/services/theme.js
@@ -1,10 +1,22 @@
 import { actions } from '~/store/theme';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const THEME_STORAGE_KEY = '@RNTEMPLATE_THEME';
 
 export const switchTheme = (theme) => async (dispatch) => {
   try {
-    //TODO: async storage here
-    console.log('SWITCH THEME THUNK');
+    await AsyncStorage.setItem(THEME_STORAGE_KEY, JSON.stringify(theme));
     dispatch(actions.switchTheme(theme));
+  } catch (e) {
+    return console.error(e.message);
+  }
+};
+
+export const getCurrentTheme = () => async (dispatch) => {
+  try {
+    const themeString = await AsyncStorage.getItem(THEME_STORAGE_KEY);
+    const theme = JSON.parse(themeString);
+    if (theme) dispatch(actions.switchTheme(theme));
   } catch (e) {
     return console.error(e.message);
   }
